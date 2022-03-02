6e8fbb0c46a7204528fdb3af934d71e4f721c215
TennCreekBridges <bridgettvalet@gmail.com>
2022-03-02 17:45:41 -0600

    push working db server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express');
+// import mysql2 package that has been installed to node_modules
+const mysql = require('mysql2');
 
 // add PORT designation and the APP expression
 const PORT = process.env.PORT || 3001;
@@ -9,21 +11,39 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// test Express.js connection via GET route
-app.get('/', (req, res) => {
-    res.json({
-        message: 'It works, snitches!!'
+// connect to mysql database
+const db = mysql.createConnection (
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root',
+        // your mysql password
+        password: 'PicoS123!',
+        database: 'election'
     });
-});
+    // console.log('Connected to the election database. Snitches.')
 
 // default response for any other (not found) request
 app.use((req, res) => {
     res.status(404).end();
 });
 
-
-
-// connection function
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
+
+// test Express.js connection via GET route
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'It worrrks!!'
+//     });
+// });
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  });
